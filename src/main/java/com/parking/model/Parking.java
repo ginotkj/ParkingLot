da06246cce7f90827296c14c925c02d27f5a41b8
Gino <gino.tkj@gmail.com>
2017-03-25 03:03:27 +0700

    Create Parking Property (slotNo, registrationNo, color) and also getter and setter

--- src/main/java/com/parking/model/Parking.java
+++ src/main/java/com/parking/model/Parking.java
@@ -1,5 +1,32 @@
 package com.parking.model;
 
 public class Parking {
+	int slotNo;
+	String registrationNo;
+	String color;
+
+	public int getSlotNo() {
+		return slotNo;
+	}
+
+	public void setSlotNo(int slotNo) {
+		this.slotNo = slotNo;
+	}
+
+	public String getRegistrationNo() {
+		return registrationNo;
+	}
+
+	public void setRegistrationNo(String registrationNo) {
+		this.registrationNo = registrationNo;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
 
 }
